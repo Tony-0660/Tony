a31563d743794ef6db1383473519c84c071d1fb4
johnolliverjr <johnolliverjr@gmail.com>
2022-02-17 15:05:39 +0300

    classes created

--- src/week2/swapTwoNumbers.java
+++ src/week2/swapTwoNumbers.java
@@ -10,13 +10,6 @@ public class swapTwoNumbers {
         b = a - b;
         a = a - b;*/
 
-        //Solution 2:
-        int  a = 10;      int  b  = 20;
-        a = a^b;
-        System.out.println(a);
-        b = a^b;
-        System.out.println(b);
-        a = a^b;
 
 
     }
