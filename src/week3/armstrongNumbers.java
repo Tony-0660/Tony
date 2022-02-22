75eaf9771e2a8fa7fa3c843ea674c42d931d405e
johnolliverjr <johnolliverjr@gmail.com>
2022-02-22 17:03:54 +0300

    classes created

---
+++ src/week3/armstrongNumbers.java
@@ -0,0 +1,22 @@
+package week3;
+
+public class armstrongNumbers {
+    /*
+    Write a method that can check if a number is Armstrong number
+     */
+    public static void main(String[] args) {
+        System.out.println(isArmStrongNumber(5));
+    }
+    public static boolean isArmStrongNumber(int num) {
+        int a = 0, b = 0, c = num;
+
+        while (num > 0) {
+            a = num % 10;
+            num /= 10;
+            b = b + (a * a * a);
+        }
+
+        return (c == b) ? true : false;
+    }
+
+}
---
+++ src/week3/primeNumber.java
@@ -0,0 +1,26 @@
+package week3;
+
+public class primeNumber {
+     /*
+        Write a method that can check if a number is prime or not
+     */
+     public static void main(String[] args) {
+         System.out.println(primeNumber(5));
+
+     }
+
+    public static boolean primeNumber(int num) {
+        if (num < 2) {
+            return false;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
