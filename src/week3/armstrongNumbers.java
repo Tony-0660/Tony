9f782537c2cbe03f370866a03c29fed684987d10
johnolliverjr <johnolliverjr@gmail.com>
2022-03-01 20:34:10 +0300

    classes created

--- src/week3/armstrongNumbers.java
+++ src/week3/armstrongNumbers.java
@@ -5,7 +5,7 @@ public class armstrongNumbers {
     Write a method that can check if a number is Armstrong number
      */
     public static void main(String[] args) {
-        System.out.println(isArmStrongNumber(5));
+        System.out.println(isArmStrongNumber(153));
     }
     public static boolean isArmStrongNumber(int num) {
         int a = 0, b = 0, c = num;
