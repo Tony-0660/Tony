3e2d205339f265652b966de1d83ee6abb96f932d
johnolliverjr <johnolliverjr@gmail.com>
2022-03-01 20:22:42 +0300

    classes created

--- src/week3/reverseNegativeNumber.java
+++ src/week3/reverseNegativeNumber.java
@@ -4,19 +4,17 @@ public class reverseNegativeNumber {
     //Write a return method that can reverse negative number and return it as int
 
     public static void main(String[] args) {
+
         System.out.println(reverseNum(-53));
     }
+
     public static int reverseNum(int  num) {
 
         String str = new StringBuilder(""+num).reverse().toString();
 
         if(num < 0) {
-
             str = "-"+str.substring(0, str.length()-1);
-
         }
-
         return Integer.valueOf(str);
-
     }
 }
