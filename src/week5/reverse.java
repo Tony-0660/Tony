636b75cb133bff2c91c705918dba22a563fc5cb4
johnolliverjr <johnolliverjr@gmail.com>
2022-03-10 00:18:05 +0300

    classes created

---
+++ src/week5/reverse.java
@@ -0,0 +1,20 @@
+package week5;
+
+public class reverse {
+   /* Write a return method that can reverse  String
+    Ex: Reverse("ABCD"); ==> DCBA*/
+   public static void main(String[] args) {
+       System.out.println(Reverse("abcd"));
+   }
+
+   public static String StrReverse(String str) {
+       String reverse="";
+       for(int i=str.length()-1; i >= 0; i--)
+           reverse += str.toCharArray()[i];
+       return  reverse;
+   }
+    public  static String  Reverse(String str) {
+        return new StringBuffer(str).reverse().toString();
+    }
+
+}
---
+++ src/week5/sortLettersNumbers.java
@@ -0,0 +1,43 @@
+package week5;
+
+import java.util.Arrays;
+
+public class sortLettersNumbers {
+    /*Given alphanumeric String, we need to split the string into substrings of consecutive letters or numbers, sort the individual string and append them back together
+Ex:
+Input:  "DC501GCCCA098911"
+OutPut: "CD015ACCCG011899"*/
+    public static void main(String[] args) {
+        System.out.println(SortLettersAndNumbersFromString("DC501GCCCA098911"));
+    }
+    public static String SortLettersAndNumbersFromString(String str) {
+
+        String str2 = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            str2 += "" + str.charAt(i);
+            if (Character.isAlphabetic(str.charAt(i)) && i < str.length() - 1) {
+                if (Character.isDigit(str.charAt(i + 1))) {
+                    str2 += ",";
+                }
+            }
+            if (Character.isDigit(str.charAt(i)) && i < str.length() - 1) {
+                if (Character.isAlphabetic(str.charAt(i + 1))) {
+                    str2 += ",";
+                }
+            }
+        }
+
+        String[] arr = str2.split(",");
+        str = "";
+
+        for (String each : arr) {
+            char[] chars = each.toCharArray();
+            Arrays.sort(chars);
+            for (char eachChar : chars) {
+                str += "" + eachChar;
+            }
+        }
+        return str;
+    }
+}
