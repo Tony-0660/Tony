6e131652fcf57e27d2eee9e1745004b4c65ef059
johnolliverjr <johnolliverjr@gmail.com>
2022-04-07 22:55:21 +0300

    classes created

--- src/from0_100.java
+++ src/from0_100.java
@@ -5,34 +5,34 @@ public class from0_100 {
 
         //solution one
         String ten = "AaronCydeo";
-        String nine= "AaronSDET";
-        int one = ten.length() - nine.length(); //10-10=0
+        String nine = "AaronSDET";
+        int one = ten.length() - nine.length(); //10-9=1
         int hundred = ten.length() * ten.length(); //10*10=100
         for (int i = one; i <= hundred; i++) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
         System.out.println("\n-----------------");
 
         //solution two
-        int anotherOne = 'A' / 'A'; //65/65=1 ASCII
+        int anotherOne = 'a' / 'a'; //97/97=1 ASCII
         for (int i = one; i <= (ten.length() * ten.length()); i++) {
-            System.out.print(i+" ");
+            System.out.print(i + " ");
         }
         System.out.println("\n-----------------");
 
         //solution three
-        for (int i = one; i <='d'; i++) { //'d'=100 ASCII
-            System.out.print(i+" ");
+        for (int i = one; i <= 'd'; i++) { //'d'=100 ASCII
+            System.out.print(i + " ");
         }
         System.out.println("\n-----------------");
 
         //solution four
         Month jan = Month.JANUARY;
-        int newOne= jan.getValue();
-        Month oct= Month.OCTOBER;
-        int newTen=oct.getValue();
-        for (int i = newOne; i <=(newTen*newTen); i++) {
-            System.out.print(i+" ");
+        int newOne = jan.getValue();
+        Month oct = Month.OCTOBER;
+        int newTen = oct.getValue();
+        for (int i = newOne; i <= (newTen * newTen); i++) {
+            System.out.print(i + " ");
         }
 
     }
