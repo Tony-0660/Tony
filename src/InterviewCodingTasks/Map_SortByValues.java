baf6e4d257044e55a36d31d2f7e58015476d8c39
johnolliverjr <johnolliverjr@gmail.com>
2022-04-15 21:22:50 +0300

    classes created

--- src/InterviewCodingTasks/Map_SortByValues.java
+++ src/InterviewCodingTasks/Map_SortByValues.java
@@ -1,17 +1,31 @@
 package InterviewCodingTasks;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Map_SortByValues {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("k0", 0);
+        map.put("k5", 5);
+        map.put("k2", 2);
+        map.put("k1", 1);
+        map.put("k3", 3);
+
+        for (String each : map.keySet()) {
+            System.out.println("key: " + each + " value: " + map.get(each));
+        }
+        System.out.println("2nd "+ map);
+        System.out.println("sortByValue(map) = " + sortByValue(map));
+
+    }
+
+
 
     /*
         Write a method that can sort the map by values
      */
 
-    public Map<String, Integer> sortByValue(Map<String, Integer> map) {
+    public static Map<String, Integer> sortByValue(Map<String, Integer> map) {
         List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
         list.sort(Map.Entry.comparingByValue());
 
