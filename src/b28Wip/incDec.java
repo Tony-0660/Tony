ebe1cff26c3090acbb4063ec00fa757f9f6a322e
Aaron <aaron@cybertekschool.com>
2022-10-26 16:07:09 +0300

    classes created

--- src/b28Wip/incDec.java
+++ src/b28Wip/incDec.java
@@ -3,7 +3,7 @@ package b28Wip;
 public class incDec {
     public static void main(String[] args) {
         int num=0;
-        num=num++;
+        num=num++;//first, starts with post inc. (right side of the = ) increase num value 1 more but only in the memory, still it's value is 0 at the moment.then goes left side of the = (assignment) sign and assign 0 value to num which overwrites the post inc. with this assignment and our num value remains 0 therefore num++ is like never used.
         num=num++;
         num=num++;
 
